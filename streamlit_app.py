e4546fc1ed1b385560feed822f6991d3f627efe8
jdhoopes96 <142526790+jdhoopes96@users.noreply.github.com>
2023-08-30 08:12:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 from streamlit_chat import message
 
-os.environ['OPENAI_API_KEY']='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'
+os.environ['OPENAI_API_KEY']=OPEN_AI_KEY
 
 #####################################################
 from langchain.chains import LLMChain
