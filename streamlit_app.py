458a03d5d82233ffc51640a115b59ffa9e31c936
jdhoopes96 <142526790+jdhoopes96@users.noreply.github.com>
2023-09-06 10:52:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,35 +70,11 @@ with chat_placeholder.container():
 with st.container():
     st.text_input("", on_change=on_input_change, key="user_input")
 
-hide_streamlit_style = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stStatusWidget"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                #MainMenu {
-                visibility: hidden;
-                height: 0%;
-                }
-                header {
-                visibility: hidden;
-                height: 0%;
-                }
-                footer {
-                visibility: hidden;
-                height: 0%;
-                }
-                </style>
-                """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+styl = f"""
+    <style>
+        .embeddedAppMetaInfoBar_container__DxxL1 {
+            visibility:hidden;
+        }
+    </style>
+    """
+    st.markdown(styl, unsafe_allow_html=True)
