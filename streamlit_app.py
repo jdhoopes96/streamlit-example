fcf637cb4cd4682c8f5276968eb11eaf9ca1c059
jdhoopes96 <142526790+jdhoopes96@users.noreply.github.com>
2023-09-06 14:02:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ from streamlit_chat import message
 
 #replace with new one and use secret method
 
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
-os.environ['OPENAI_API_KEY']=OPENAI_API_KEY
+#OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+#os.environ['OPENAI_API_KEY']=OPENAI_API_KEY
 
 
 #####################################################
