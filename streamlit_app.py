417bf844ac8bc6cf04ee140b4b906572f9225e95
jdhoopes96 <142526790+jdhoopes96@users.noreply.github.com>
2023-09-06 13:18:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from streamlit_chat import message
 
 #replace with new one and use secret method
 
-os.environ['OPENAI_API_KEY']={OPENAI_API_KEY}
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
 
 #####################################################
 from langchain.chains import LLMChain
